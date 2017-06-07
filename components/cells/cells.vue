0dbb854639fdcb0f07bfaf81a525ba7d7e624005
TY-089\Administrator <lmw6412036@sina.com>
2017-06-07 14:41:34 +0800

    初始化

---
+++ components/cells/cells.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+
+        },
+        data() {
+            return {
+
+            };
+        },
+        mounted() {
+
+        },
+        beforeDestroy() {
+
+        },
+        methods: {
+
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+
+</style>
