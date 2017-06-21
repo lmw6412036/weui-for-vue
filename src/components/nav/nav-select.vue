ba8cb3296cbfd6c362b64d376bd52878d5d2fc00
lmw6412036 <lmw6412036@sina.com>
2017-06-22 01:02:07 +0800

    增加navselect组件

---
+++ src/components/nav/nav-select.vue
@@ -0,0 +1,74 @@
+<template>
+    <div ref="navSelect" class="nav-select">
+        <span>选择</span>
+        <div ref="wrap" class="wrap">
+            <div class="mask"></div>
+            <div class="main"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {timeFormat} from "d3-time-format"
+    import IScroll from 'iscroll/build/iscroll-probe'
+    export default {
+        data() {
+            return {};
+        },
+        computed: {},
+        components: {},
+        mounted() {
+            var hhh = window.innerHeight;
+            var www = window.innerWidth;
+            var h = this.$refs.navSelect.clientHeight;
+            this.$refs.wrap.style.top = h + "px";
+        },
+        beforeDestroy() {
+
+        },
+        methods: {}
+    };
+</script>
+
+<style scoped lang="scss">
+    .nav-select {
+        position: relative;
+        height: 45px;
+        line-height: 45px;
+        span {
+
+        }
+        .wrap {
+            position: fixed;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            .mask,.main{
+                position: absolute;
+                left: 0;
+                right: 0;
+                top: 0;
+            }
+            .mask{
+                bottom: 0;
+                background-color: rgba(0,0,0,0.5);
+            }
+            .main{
+                height: 70%;
+                overflow: hidden;
+                background-color: white;
+            }
+        }
+        &::after {
+            margin-left: 5px;
+            border-left: 5px solid transparent;
+            border-right: 5px solid transparent;
+            border-top: 10px solid #999;
+            display: inline-block;
+            content: "";
+            //position: absolute;
+            height: 0;
+            width: 0;
+        }
+    }
+</style>
--- src/pages/demo.vue
+++ src/pages/demo.vue
@@ -1,48 +1,47 @@
 <template>
-  <div class="weui-cells weui-cells_form">
-    <date-picker label="出生日期"></date-picker>
-  </div>
+    <nav-select></nav-select>
 </template>
 <script>
-  import FormButton from "../components/form/button.vue"
-  import Confirm from "../components/dialog/confirm.vue"
-  import Alert from "../components/dialog/alert.vue"
-  import Toast from "../components/toast.vue"
-  import Loading from "../components/loading.vue"
-  import Msg from "../components/msg.vue"
-  import FormInput from "../components/form/input.vue"
-  import SelectPicker from "../components/form/select-picker.vue"
-  import DatePicker from "../components/form/date-picker.vue"
+    import FormButton from "../components/form/button.vue"
+    import Confirm from "../components/dialog/confirm.vue"
+    import Alert from "../components/dialog/alert.vue"
+    import Toast from "../components/toast.vue"
+    import Loading from "../components/loading.vue"
+    import Msg from "../components/msg.vue"
+    import FormInput from "../components/form/input.vue"
+    import SelectPicker from "../components/form/select-picker.vue"
+    import DatePicker from "../components/form/date-picker.vue"
+    import NavSelect from "../components/nav/nav-select.vue"
 
-  export default {
-    data() {
-      return {
-        data: [],
-        form: {
-          value: ""
+    export default {
+        data() {
+            return {
+                data: [],
+                form: {
+                    value: ""
+                },
+                disabled: true,
+                show: false
+            };
         },
-        disabled: true,
-        show: false
-      };
-    },
-    computed: {},
-    components: {FormButton, Msg, Loading, Toast, FormInput, Alert, Confirm, SelectPicker,DatePicker},
-    mounted() {
-      setTimeout((res) => {
-        this.show = true;
-      }, 5000)
-    },
-    beforeDestroy() {
-    },
-    methods: {
-      ok(){
-        alert("ok")
-      },
-      cancel(){
-        alert("cancel")
-      }
-    }
-  };
+        computed: {},
+        components: {FormButton, Msg, Loading, Toast, FormInput, Alert, Confirm, SelectPicker, DatePicker, NavSelect},
+        mounted() {
+            setTimeout((res) => {
+                this.show = true;
+            }, 5000)
+        },
+        beforeDestroy() {
+        },
+        methods: {
+            ok(){
+                alert("ok")
+            },
+            cancel(){
+                alert("cancel")
+            }
+        }
+    };
 </script>
 
 <style scoped lang="scss">
