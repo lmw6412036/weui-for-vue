441aacd5c0aab54ccaea19ccf358ab87fe518fdd
lmw6412036 <lmw6412036@sina.com>
2017-06-26 23:49:49 +0800

    增加uploader组件

---
+++ src/pages/form/uploader.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {};
+        },
+        computed: {},
+        components: {},
+        mounted() {
+
+        },
+        beforeDestroy() {
+
+        },
+        methods: {}
+    };
+</script>
+
+<style scoped lang="scss">
+
+</style>
