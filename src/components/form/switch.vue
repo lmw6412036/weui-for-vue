3a483c615a80a604e25fffb815caf4580afe3add
lmw6412036 <lmw6412036@sina.com>
2017-06-29 23:04:39 +0800

    增加switch组件

---
+++ src/components/form/switch.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="weui-cell weui-cell_switch">
+        <div class="weui-cell__bd">{{label}}</div>
+        <div class="weui-cell__ft">
+            <input v-model="open" class="weui-switch" type="checkbox">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            label:String,
+            value:Boolean
+        },
+        data() {
+            return {
+                open:this.value
+            };
+        },
+        computed: {},
+        components: {},
+        mounted() {
+
+        },
+        beforeDestroy() {
+
+        },
+        methods: {},
+        watch:{
+            open(value){
+                this.$emit("input",value);
+            }
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/pages/form/code.vue
+++ src/pages/form/code.vue
@@ -1,50 +1,55 @@
 <template>
-  <div class="weui-cells weui-cells_form">
-    <form-code @code:ok="countdown=true" @code:countdownend="countdown=false" @code:error="error" :countdown="countdown"
-               v-model="code" default-time="10" label="验证码" :mobile="mobile"></form-code>
+    <div class="weui-cells weui-cells_form">
+        <form-code @code:ok="countdown=true" @code:countdownend="countdown=false" @code:error="error"
+                   :countdown="countdown"
+                   v-model="code" default-time="10" label="验证码" :mobile="mobile"></form-code>
 
-    <form-input label="姓名" v-model="code"></form-input>
-    <pre>{{$v.code}}</pre>
-  </div>
+        <form-input label="姓名" v-model="name"></form-input>
+        <form-switch label="开关" v-model="open"></form-switch>
+
+    </div>
 </template>
 
 <script>
-  import FormInput from "../../components/form/input.vue"
-  import FormCode from "../../components/form/code.vue"
-  import {required} from "vuelidate/lib/validators"
-  import regex from "../../lib/regex"
+    import FormSwitch from "../../components/form/switch.vue"
+    import FormInput from "../../components/form/input.vue"
+    import FormCode from "../../components/form/code.vue"
+    import {required} from "vuelidate/lib/validators"
+    import regex from "../../lib/regex"
 
-  export default {
-    data() {
-      return {
-        code: "123",
-        countdown: false,
-        mobile: 15868823516
-      };
-    },
-    validations: {
-      code: {
-        required,
-        regex:regex("ldldll")
-      }
-    },
-    computed: {},
-    components: {FormInput, FormCode},
-    mounted() {
+    export default {
+        data() {
+            return {
+                name: "lmw",
+                code: "123",
+                open:true,
+                countdown: false,
+                mobile: 15868823516
+            };
+        },
+        validations: {
+            code: {
+                required,
+                regex: regex("ldldll")
+            }
+        },
+        computed: {},
+        components: {FormSwitch,FormInput, FormCode},
+        mounted() {
 
-    },
-    beforeDestroy() {
+        },
+        beforeDestroy() {
 
-    },
-    methods: {
-      error(){
+        },
+        methods: {
+            error(){
 
-      },
-      send(){
+            },
+            send(){
 
-      }
-    }
-  };
+            }
+        }
+    };
 </script>
 
 <style scoped lang="scss">
